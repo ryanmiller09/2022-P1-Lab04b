dee97e6553a5fdd26c2b5703ed27bf5105182a00
13195 <ryan.miller@vtsd.com>
2022-10-24 07:41:18 -0400

    Lab 4b

---
+++ Temp.java
@@ -0,0 +1,13 @@
+public class Temp {
+    public static void main(String args[]){
+
+        for (int k=0; k<=100; k+=3){
+        int sum = 100 - k;
+        System.out.print(sum + ", ");
+
+
+
+        }
+
+    }
+}
